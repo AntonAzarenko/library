979e6719c802c53cae713619fc98d748975edb33
AntonAzarenko <ant-azarenko@mail.ru>
2018-08-25 19:49:20 +0300

    restore

--- src/library/mvc/AuthorController.java
+++ src/library/mvc/AuthorController.java
@@ -1,10 +1,12 @@
 package library.mvc;
 
+import library.entity.Author;
 import library.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -41,4 +43,57 @@ public class AuthorController {
         return "redirect:editAuthors";
     }
 
+    /**
+     * This methos remove author by Id and redirect to allauthos.jsp
+     *
+     * @param id
+     * @return
+     */
+    @PostMapping(value = "removeAuthors.html")
+    public String delete(@RequestParam("id") final Long id) {
+        service.delete(id);
+        return "redirect:authors.html";
+    }
+
+    /**
+     * This method gets authors by name, put them to model and redirect on author.jsp
+     *
+     * @param model
+     * @param name
+     * @return
+     */
+    @GetMapping(value = "getAuthorByName.html")
+    public String getByName(final Model model, @RequestParam("name") final String name) {
+        model.addAttribute("author", service.getByName(name));
+        return "author";
+    }
+
+    /**
+     * This method gets authors by ID, put them to model and redirect on author.jsp
+     *
+     * @param model
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "getAuthorById.html")
+    public String getById(final Model model, @RequestParam("id") final Long id) {
+        model.addAttribute("author", service.getById(id));
+        return "author";
+    }
+
+    /**
+     * This method saves authors  and redirect on allAuthors.jsp
+     *
+     * @param id
+     * @param name
+     * @return
+     */
+    @PostMapping(value = "saveAuthor.html")
+    public String save(@RequestParam(required = false) @PathVariable("id") final Long id,
+                       @RequestParam("name") final String name) {
+        service.save(new Author(id, name));
+        return "allauthors";
+    }
 }
+
+
