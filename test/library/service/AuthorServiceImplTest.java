7eee1d939e4e7c38692a79aa2a5cc3caec2e8d92
AntonAzarenko <ant-azarenko@mail.ru>
2018-08-25 08:01:34 +0300

    add spring test and junit test

---
+++ test/library/service/AuthorServiceImplTest.java
@@ -0,0 +1,37 @@
+package library.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@ContextConfiguration({
+        "classpath:context.xml"
+})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AuthorServiceImplTest {
+
+    @Test
+    public void setAuthorDao() {
+    }
+
+    @Test
+    public void getById() {
+    }
+
+    @Test
+    public void getAllAuthors() {
+    }
+
+    @Test
+    public void save() {
+    }
+
+    @Test
+    public void getAuthorByName() {
+    }
+
+    @Test
+    public void delete() {
+    }
+}
