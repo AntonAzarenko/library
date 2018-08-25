2d81f0375fb211b44cbf8c25ff2f0861a948a185
Dzmitry Ivanou <pogas89@gmail.com>
2018-08-25 15:58:46 +0300

    fix changelog + add info to author test

--- test/library/service/AuthorServiceImplTest.java
+++ test/library/service/AuthorServiceImplTest.java
@@ -1,9 +1,15 @@
 package library.service;
 
+import library.entity.Author;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+import static library.AuthorTestData.*;
 
 @ContextConfiguration({
         "classpath:context.xml"
@@ -11,12 +17,17 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class AuthorServiceImplTest {
 
+    @Autowired
+    private  AuthorService service;
+
     @Test
     public void getById() {
     }
 
     @Test
     public void getAllAuthors() {
+        List<Author> authors = service.getAll();
+        assertMatch(authors,AUTHOR1,AUTHOR2,AUTHOR3);
     }
 
     @Test
