cb3ad9b9878535bf394785f7ff710b5c09b16d1c
AntonAzarenko <ant-azarenko@mail.ru>
2018-08-25 20:14:23 +0300

    add empty Publisher Controller

---
+++ src/library/mvc/PublisherController.java
@@ -0,0 +1,13 @@
+package library.mvc;
+
+import library.service.PublisherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PublisherController {
+
+    private PublisherService service;
+
+
+}
