7b640078104c965293ca1c508c34e438bea538bc
AntonAzarenko <ant-azarenko@mail.ru>
2018-08-25 20:49:29 +0300

    fix

--- src/library/entity/Author.java
+++ src/library/entity/Author.java
@@ -1,5 +1,7 @@
 package library.entity;
 
+import java.util.Objects;
+
 public class Author extends Entity {
 
     private static final long serialVersionUID = 1L;
@@ -27,4 +29,16 @@ public class Author extends Entity {
         this.name = name;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(getId(), author.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
 }
