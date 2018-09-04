30f112e030c73d0fff5e7dd470358c62cb6c0b73
AntonAzarenko <ant-azarenko@mail.ru>
2018-09-05 00:15:31 +0300

    fix

--- src/library/mvc/MetaDataController.java
+++ src/library/mvc/MetaDataController.java
@@ -20,18 +20,14 @@ public class MetaDataController {
             try {
                 byte[] bytes = file.getBytes();
                 name = file.getOriginalFilename();
-                String root = "";
-                File dir = new File(root + File.separator + "zip");
-                if (dir.exists()) {
-                    dir.mkdirs();
-                }
+                File dir = new File( File.separator + "zip");
                 File uploadedFile = new File(dir.getAbsolutePath() + File.separator + name);
                 BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(uploadedFile));
                 stream.write(bytes);
                 stream.flush();
                 stream.close();
             } catch (IOException e) {
-                e.printStackTrace();
+                return "error";
             }
         }
         return "addedit";
